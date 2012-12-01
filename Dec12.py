1d3956994d1550b35c53589dde726e394fb33b2e
Ben Andken <ben.andken@gmail.com>
2012-11-30 21:13:00 -0500

    initial commit

---
+++ Dec12.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+#Ponder This Challenge:
+#36 people live in a 6x6 grid, and each one of them lives in a separate square of the grid. Each resident's neighbors are those who live in the squares that have a common edge with that resident's square.
+#Each resident of the grid is assigned a natural number N, such that if a person receives some N>1, then he or she must also have neighbors that have been assigned all of the numbers 1,2,...,N-1.
+#Find a configuration of the 36 neighbors where the sum of their numbers is at least 90.
+#Finding a sum of more than 90 will earn you a star near your name.
+#As an example, the highest sum we can get in a 3x3 grid is 20:
+#1 2 1
+#4 3 4
+#2 1 2
+
