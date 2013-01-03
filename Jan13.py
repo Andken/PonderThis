ee44289b8c342a9a5f5acdf16a8df6f0ef0355af
Ben Andken <ben.andken@gmail.com>
2013-01-02 22:45:06 -0500

    generating all permutations and checking subsequences

--- Jan13.py
+++ Jan13.py
@@ -12,12 +12,36 @@
 # Find 13 permutations of the letters ABCEFHIJLMNPQTVXYZ which satisfy the same condition: for every three letters and every one of their six possible orders, there exists at least one permutation in which the letters appear in this order.
 # A bonus question: What is special about this set of letters?
 
-letters = set(['A', 'B', 'C', 'E', 'F', 'H', 'I', 'J', 'L', 'M', 'N', 'P', 'Q', 'T', 'V', 'X', 'Y', 'Z'])
+import itertools
 
-combos_of_three_letters = []
-for a in letters:
-    new_letters = (list(set(letters) - set([a])))
-    print new_letters
+def isSubsequence(sub, str):
+    start_index = 0
+    for letter in sub:
+        start_index = str.find(letter, start_index) + 1
+        if start_index == 0: 
+            return False
+    return True
+        
+
+#letters = ['E', 'I', 'S', 'H']
+#letters = ['A', 'B', 'C', 'E', 'F', 'H', 'I', 'J', 'L', 'M', 'N', 'P', 'Q', 'T', 'V', 'X', 'Y', 'Z']
+letters = "ABCEFHIJLMNPQTVXYZ"
+combos_of_three_letters = list(itertools.combinations(letters, 3))
+permutes_of_three_letters = list(itertools.permutations(letters, 3))
+
+permutes_of_answer = []
+permutes_of_answer.append(letters)
+
+answers = dict(zip(list(itertools.permutations(letters, 3)), [False]*len(list(itertools.permutations(letters, 3)))))
 
 
+for p in list(itertools.permutations(letters, 3)):
+    if isSubsequence(p, letters):
+        print "%c%c%c" % (p[0], p[1], p[2])
+    else:
+        print "nope %c%c%c" % (p[0], p[1], p[2])
 
+
+# 4896 different permutations must match
+# 18 letters * 13 permutations = 234 ... probably useless
+# (18 choose 3) * 13  = 10608, so there might be many solutions
